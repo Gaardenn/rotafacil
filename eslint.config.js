cc19ea37ed08049853e5e9aee126b31b9349b031
Garden <nogamegarden@gmail.com>
2026-07-28 00:02:21 -0300

    inicializado repositorio e primeiro commit do projeto

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+import globals from 'globals'
+import js from '@eslint/js'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+
+export default [
+  { ignores: ['dist', 'dev-dist'] },
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOption: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'no-unused-vars': ['warn', { varsIgnorePattern: '^[A-Z_]' }],
+    },
+  },
+]
